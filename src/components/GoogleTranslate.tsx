684eea5a3eebc0cf4032dacb588644c666e15163
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2026-04-05 15:43:15 +0000

    Add files via upload

--- src/components/GoogleTranslate.tsx
+++ src/components/GoogleTranslate.tsx
@@ -1,49 +1,31 @@
-
 'use client'
-export {}
 
 import { useEffect } from 'react'
 
 declare global {
   interface Window {
-    googleTranslateElementInit?: () => void
-    google?: any
     __gtPatched?: boolean
   }
 }
 
 export default function GoogleTranslate() {
   useEffect(() => {
-    // Patch DOM to prevent React crashes from Google Translate mutations
-    'use client'
-
-    import { useEffect } from 'react'
-
-    declare global {
-      interface Window {
-        __gtPatched?: boolean
+    if (!window.__gtPatched) {
+      const origRemoveChild = Node.prototype.removeChild
+      Node.prototype.removeChild = function <T extends Node>(child: T): T {
+        if (child.parentNode !== this) return child
+        return origRemoveChild.call(this, child) as T
       }
+
+      const origInsertBefore = Node.prototype.insertBefore
+      Node.prototype.insertBefore = function <T extends Node>(newNode: T, refNode: Node | null): T {
+        if (refNode && refNode.parentNode !== this) return newNode
+        return origInsertBefore.call(this, newNode, refNode) as T
+      }
+
+      window.__gtPatched = true
     }
+  }, [])
 
-    export default function GoogleTranslate() {
-      useEffect(() => {
-        // Patch DOM to prevent React crashes from Google Translate mutations
-        if (!window.__gtPatched) {
-          const origRemoveChild = Node.prototype.removeChild
-          Node.prototype.removeChild = function <T extends Node>(child: T): T {
-            if (child.parentNode !== this) return child
-            return origRemoveChild.call(this, child) as T
-          }
-
-          const origInsertBefore = Node.prototype.insertBefore
-          Node.prototype.insertBefore = function <T extends Node>(newNode: T, refNode: Node | null): T {
-            if (refNode && refNode.parentNode !== this) return newNode
-            return origInsertBefore.call(this, newNode, refNode) as T
-          }
-
-          window.__gtPatched = true
-        }
-      }, [])
-
-      return <div id="google_translate_element" />
-    }
+  return <div id="google_translate_element" />
+}
